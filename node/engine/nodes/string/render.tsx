bf8e82a9765cb0e79f4115e83f1157771f84881f
Christoph Franke <krito@krito.de>
2020-04-07 21:12:55 +0200

    renamed string into text node

---
+++ node/engine/nodes/string/render.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { observer } from 'mobx-react'
+import { RenderProps } from '@engine/types'
+
+export default observer((props: RenderProps) => <>
+  {props.params.value}
+</>)
