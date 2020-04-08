fcbf13fea1e7c8718ebea0ac9ff619bcc0c7bc8e
Christoph Franke <krito@krito.de>
2020-04-09 01:10:50 +0200

    type description implemented

--- node/shared/type-display.ts
+++ node/shared/type-display.ts
@@ -4,8 +4,16 @@ export function describe(type: ValueType): string {
   return type.display.split(/(\{.*\})/)
     .map(match => {
       if (match[0] === '{' && match[match.length - 1] === '}') {
-        console.warn('type description not complete yet')
-        return match
+        const key = match.substring(1, match.length - 1)
+        if (!key) {
+          return "{ " 
+            + Object.entries(type.params).map(([key, type]) => `key: ${describe(type)}`).join(', ')
+            + " }"
+        } else if (type.params[key]) {
+          return describe(type.params[key])
+        } else {
+          return `{"${key}" not found}`
+        }
       } else {
         return match
       }
