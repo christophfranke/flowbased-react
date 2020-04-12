65aea77760a7383148e858ed6e1ad61a327cda87
Christoph Franke <krito@krito.de>
2020-04-12 17:57:26 +0200

    memoize render, value and type functions

--- node/engine/render.ts
+++ node/engine/render.ts
@@ -1,4 +1,5 @@
 import React from 'react'
+import { computedFn } from 'mobx-utils'
 import { Node, ValueType, Scope } from '@engine/types'
 
 import Nodes from '@engine/nodes'
@@ -6,30 +7,30 @@ import * as TypeDefinition from '@engine/type-definition'
 import { matchType } from '@engine/type-functions'
 import { getGlobalScope, entries } from '@engine/scopes'
 
-export function value(node: Node, scope: Scope): any {
+export const value = computedFn(function(node: Node, scope: Scope): any {
   return Nodes[node.name].resolve(node, scope)
-}
+})
 
-export function render(node: Node): any {
+export const render = computedFn(function(node: Node): any {
   return value(node, getGlobalScope())
-}
+})
 
-export function unmatchedType(node: Node): ValueType {
+export const unmatchedType = computedFn(function(node: Node): ValueType {
   return Nodes[node.name].type.output(node)
-}
+})
 
-export function type(node: Node): ValueType {
+export const type = computedFn(function(node: Node): ValueType {
   return node.connections.output.reduce(
     (resultType, connection) => {
       return matchType(resultType, expectedType(connection.node, connection.key, node))
     },
     unmatchedType(node)
   )
-}
+})
 
-export function numScopeResolvers(node: Node): number {
+export const numScopeResolvers = computedFn(function (node: Node): number {
   return entries(node).length
-}
+})
 
 export function expectedType(node: Node, key: string = '', other?: Node): ValueType {
   return key
